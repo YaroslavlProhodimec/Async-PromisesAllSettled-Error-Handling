e1027c694f2c5491154a9db9964780f3a285be04
TonyMyrato <tonymyrato@TonyMyratos-Air.MyMiFi>
2024-02-18 00:45:14 -0800

    solution

---
+++ Async-PromisesAllSettled-Error-Handling.ts
@@ -0,0 +1,17 @@
+// Async-PromisesAllSettled-Error-Handling
+const promise1 = Promise.resolve('First')
+const promise2 = Promise.resolve('Second')
+const promise3 = Promise.reject('Third')
+const promise4 = Promise.resolve('Fourth')
+
+const  runPromises = async () => {
+    const res1 = await Promise.allSettled([promise1,promise2])
+    const res2 = await Promise.allSettled([promise3,promise4])
+    return [res1,res2]
+}
+
+
+runPromises()
+    .then(res => console.log(res))
+        .catch(err=>console.log(err))
+
